6644e9980575279669052dbb43306892d7d405c5
LIU JIAHAO <1205965e@gmail.com>
2017-03-20 21:55:50 +0800

    Create class EarliestComparator

---
+++ src/main/java/utask/commons/comparators/EarliestComparator.java
@@ -0,0 +1,17 @@
+//@@author A0138493W
+
+package utask.commons.comparators;
+
+import java.util.Comparator;
+
+import utask.model.task.ReadOnlyTask;
+
+public class EarliestComparator implements Comparator<ReadOnlyTask>{
+
+    @Override
+    public int compare(ReadOnlyTask o1, ReadOnlyTask o2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
