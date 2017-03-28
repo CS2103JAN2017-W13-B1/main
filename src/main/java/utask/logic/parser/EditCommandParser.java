2af89104e5a39ef26ba6439def4f112fbd6ae391
Denver Tan <revned.nat90@gmail.com>
2017-03-28 21:07:58 +0800

    Update parser to parse and read for "-" in deadline and timestamp, to be marked for removal

--- src/main/java/utask/logic/parser/EditCommandParser.java
+++ src/main/java/utask/logic/parser/EditCommandParser.java
@@ -8,6 +8,7 @@ import static utask.logic.parser.CliSyntax.PREFIX_NAME;
 import static utask.logic.parser.CliSyntax.PREFIX_TAG;
 import static utask.logic.parser.CliSyntax.PREFIX_TIMESTAMP;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -19,57 +20,64 @@ import utask.logic.commands.EditCommand;
 import utask.logic.commands.EditCommand.EditTaskDescriptor;
 import utask.logic.commands.IncorrectCommand;
 import utask.model.tag.UniqueTagList;
-import utask.model.task.Deadline;
-import utask.model.task.Timestamp;
 
 /**
  * Parses input arguments and creates a new EditCommand object
  */
-//@@author A0138423J
+// @@author A0138423J
 public class EditCommandParser {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the EditCommand
-     * and returns an EditCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the
+     * EditCommand and returns an EditCommand object for execution.
      */
     public Command parse(String args) {
         assert args != null;
-        ArgumentTokenizer argsTokenizer =
-                new ArgumentTokenizer(PREFIX_NAME, PREFIX_DEADLINE, PREFIX_TIMESTAMP,
-                        PREFIX_FREQUENCY, PREFIX_TAG, PREFIX_DONE);
+        ArgumentTokenizer argsTokenizer = new ArgumentTokenizer(PREFIX_NAME,
+                PREFIX_DEADLINE, PREFIX_TIMESTAMP, PREFIX_FREQUENCY, PREFIX_TAG,
+                PREFIX_DONE);
         argsTokenizer.tokenize(args);
-        List<Optional<String>> preambleFields = ParserUtil.splitPreamble(argsTokenizer.getPreamble().orElse(""), 2);
+        List<Optional<String>> preambleFields = ParserUtil
+                .splitPreamble(argsTokenizer.getPreamble().orElse(""), 2);
 
-        Optional<Integer> index = preambleFields.get(0).flatMap(ParserUtil::parseIndex);
+        Optional<Integer> index = preambleFields.get(0)
+                .flatMap(ParserUtil::parseIndex);
         if (!index.isPresent()) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
+            return new IncorrectCommand(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
         }
+        // creates list of int to store to indicate which attribute to remove
+        // 0 : Deadline, 1 : Timestamp
+        ArrayList<Integer> attributesToRemove = new ArrayList<Integer>();
 
         EditTaskDescriptor editTaskDescriptor = new EditTaskDescriptor();
         try {
-            editTaskDescriptor.setName(ParserUtil.parseName(argsTokenizer.getValue(PREFIX_NAME)));
-
-
-            System.out.println(argsTokenizer.getValue(PREFIX_DEADLINE).toString());
-            System.out.println(!argsTokenizer.tryGet(PREFIX_DEADLINE).isEmpty());
-
+            editTaskDescriptor.setName(
+                    ParserUtil.parseName(argsTokenizer.getValue(PREFIX_NAME)));
+            editTaskDescriptor.setDeadline(ParserUtil
+                    .parseDeadline(argsTokenizer.getValue(PREFIX_DEADLINE)));
+            // checks whether @param from @PREFIX_DEADLINE is "-"
+            // if so, adds 0 to list of attributesToRemove
             if (!argsTokenizer.tryGet(PREFIX_DEADLINE).isEmpty()) {
-                editTaskDescriptor.setDeadline(ParserUtil.parseDeadline(argsTokenizer.getValue(PREFIX_DEADLINE)));
-            } else {
-                editTaskDescriptor.setDeadline(Optional.of(Deadline.getEmptyDeadline()));
+                if (argsTokenizer.tryGet(PREFIX_DEADLINE).equals("-")) {
+                    attributesToRemove.add(0);
+                }
             }
-
-            System.out.println(argsTokenizer.getValue(PREFIX_TIMESTAMP).toString());
-            System.out.println(!argsTokenizer.tryGet(PREFIX_TIMESTAMP).isEmpty());
+            editTaskDescriptor.setTimeStamp(ParserUtil
+                    .parseTimestamp(argsTokenizer.getValue(PREFIX_TIMESTAMP)));
+            // checks whether @param from @PREFIX_TIMESTAMP is "-"
+            // if so, adds 1 to list of attributesToRemove
             if (!argsTokenizer.tryGet(PREFIX_TIMESTAMP).isEmpty()) {
-                editTaskDescriptor.setTimeStamp(ParserUtil.parseTimestamp(argsTokenizer.getValue(PREFIX_TIMESTAMP)));
-            } else {
-                editTaskDescriptor.setTimeStamp(Optional.of(Timestamp.getEmptyTimestamp()));
+                if (argsTokenizer.tryGet(PREFIX_TIMESTAMP).equals("-")) {
+                    attributesToRemove.add(1);
+                }
             }
-
-            editTaskDescriptor.setFrequency(ParserUtil.parseFrequency(argsTokenizer.getValue(PREFIX_FREQUENCY)));
-            editTaskDescriptor.setTags(parseTagsForEdit(ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))));
-            editTaskDescriptor.setIsCompleted(ParserUtil.parseIsCompleted(argsTokenizer.getValue(PREFIX_DONE)));
+            editTaskDescriptor.setFrequency(ParserUtil
+                    .parseFrequency(argsTokenizer.getValue(PREFIX_FREQUENCY)));
+            editTaskDescriptor.setTags(parseTagsForEdit(
+                    ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))));
+            editTaskDescriptor.setIsCompleted(ParserUtil
+                    .parseIsCompleted(argsTokenizer.getValue(PREFIX_DONE)));
         } catch (IllegalValueException ive) {
             System.out.println(ive.toString());
             return new IncorrectCommand(ive.getMessage());
@@ -79,21 +87,25 @@ public class EditCommandParser {
             return new IncorrectCommand(EditCommand.MESSAGE_NOT_EDITED);
         }
 
-        return new EditCommand(index.get(), editTaskDescriptor);
+        return new EditCommand(index.get(), editTaskDescriptor,
+                attributesToRemove);
     }
 
     /**
-     * Parses {@code Collection<String> tags} into an {@code Optional<UniqueTagList>} if {@code tags} is non-empty.
-     * If {@code tags} contain only one element which is an empty string, it will be parsed into a
-     * {@code Optional<UniqueTagList>} containing zero tags.
+     * Parses {@code Collection<String> tags} into an
+     * {@code Optional<UniqueTagList>} if {@code tags} is non-empty. If
+     * {@code tags} contain only one element which is an empty string, it will
+     * be parsed into a {@code Optional<UniqueTagList>} containing zero tags.
      */
-    private Optional<UniqueTagList> parseTagsForEdit(Collection<String> tags) throws IllegalValueException {
+    private Optional<UniqueTagList> parseTagsForEdit(Collection<String> tags)
+            throws IllegalValueException {
         assert tags != null;
 
         if (tags.isEmpty()) {
             return Optional.empty();
         }
-        Collection<String> tagSet = tags.size() == 1 && tags.contains("") ? Collections.emptySet() : tags;
+        Collection<String> tagSet = tags.size() == 1 && tags.contains("")
+                ? Collections.emptySet() : tags;
         return Optional.of(ParserUtil.parseTags(tagSet));
     }
 
