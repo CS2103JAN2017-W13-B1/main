393b5b00e18622c9ed25757c5ce400331ffce809
LIU JIAHAO <1205965e@gmail.com>
2017-04-02 23:48:05 +0800

    Use mix test cases

--- src/test/java/utask/logic/parser/ParserUtilTest.java
+++ src/test/java/utask/logic/parser/ParserUtilTest.java
@@ -16,43 +16,34 @@ public class ParserUtilTest {
     @Test
     public void parse_index() {
         Optional<Integer> index = ParserUtil.parseIndex(" ");
-        assertTrue (!index.isPresent());
+        assertFalse (index.isPresent());
 
-        index = ParserUtil.parseIndex("%");
-        assertTrue (!index.isPresent());
-
-        index = ParserUtil.parseIndex("abc");
-        assertTrue (!index.isPresent());
+        index = ParserUtil.parseIndex("-1");
+        assertFalse (index.isPresent());
 
         index = ParserUtil.parseIndex("16");
-        assertFalse (!index.isPresent());
+        assertTrue (index.isPresent());
 
     }
 
     @Test
     public void parse_multiple_indexes() {
         Optional<List<Integer>> indexList = ParserUtil.parseMultiIndex(" ");
-        assertTrue (!indexList.isPresent());
+        assertFalse (indexList.isPresent());
 
-        indexList = ParserUtil.parseMultiIndex(" , 1");
-        assertTrue (!indexList.isPresent());
-
-        indexList = ParserUtil.parseMultiIndex("1, a");
-        assertTrue (!indexList.isPresent());
+        indexList = ParserUtil.parseMultiIndex("-1, 2");
+        assertFalse (indexList.isPresent());
 
         indexList = ParserUtil.parseMultiIndex("1, 2 to c");
-        assertTrue (!indexList.isPresent());
+        assertFalse (indexList.isPresent());
 
-        indexList = ParserUtil.parseMultiIndex("1, 1");
-        assertFalse (!indexList.isPresent());
-        assertEquals ("1", indexList.get().get(0).toString());
-
-        indexList = ParserUtil.parseMultiIndex(" 1, 1 to 3");
-        assertFalse (!indexList.isPresent());
+        indexList = ParserUtil.parseMultiIndex("1, 1, 1 to 3, 4");
         ArrayList<Integer> expectedList = new ArrayList<Integer>();
+        expectedList.add(4);
         expectedList.add(3);
         expectedList.add(2);
         expectedList.add(1);
+        assertTrue (indexList.isPresent());
         assertEquals (expectedList, indexList.get());
     }
 
@@ -63,6 +54,6 @@ public class ParserUtilTest {
         } else {
             assertTrue (ParserUtil.isPathValid("/Users/James/Desktop"));
         }
-
     }
+
 }
