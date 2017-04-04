cdcf33703dc0631eeb61de4fc06b367bbbacc0d0
LIU JIAHAO <1205965e@gmail.com>
2017-04-04 22:08:27 +0800

    Create AliasCommand

---
+++ src/main/java/utask/logic/commands/AliasCommand.java
@@ -0,0 +1,15 @@
+//@@author A0138493W
+
+package utask.logic.commands;
+
+import utask.logic.commands.exceptions.CommandException;
+
+public class AliasCommand extends Command {
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
