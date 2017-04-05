872fcaa7740b3155b82c890e22bb98eb8a68b32f
Yong Hao TENG <yonghao.teng@gmail.com>
2017-04-05 09:03:22 +0800

    add logging to dialogs

--- src/main/java/utask/ui/dialogs/UTDialog.java
+++ src/main/java/utask/ui/dialogs/UTDialog.java
@@ -1,22 +1,28 @@
 package utask.ui.dialogs;
 
+import java.util.logging.Logger;
+
 import com.google.common.eventbus.Subscribe;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialog.DialogTransition;
 import com.jfoenix.controls.JFXDialogLayout;
 
-import javafx.event.ActionEvent;
 import javafx.scene.layout.StackPane;
 import utask.commons.core.EventsCenter;
+import utask.commons.core.LogsCenter;
+import utask.staging.ui.UTMainWindow;
 import utask.staging.ui.events.KeyboardEscapeKeyPressedEvent;
 
 public abstract class UTDialog {
-    protected final JFXDialog dialog;
+    private static final Logger logger = LogsCenter.getLogger(UTMainWindow.class);
+    private final JFXDialog dialog;
     protected final JFXDialogLayout contentLayout;
     private boolean isShowing = false;
 
     public UTDialog(StackPane parent) {
+        assert parent != null : "Parent cannot be null";
+
         dialog = new JFXDialog();
         contentLayout = new JFXDialogLayout();
         initialize(parent);
@@ -31,7 +37,9 @@ public abstract class UTDialog {
 
         JFXButton closeButton = new JFXButton("CLOSE [ESC]");
         closeButton.setStyle("-fx-text-fill: -fx-utask-accentcolor");
-        closeButton.setOnAction(this::onClose);
+        closeButton.setOnAction((event) -> {
+            onClose();
+        });
 
         contentLayout.setActions(closeButton);
     }
@@ -39,11 +47,12 @@ public abstract class UTDialog {
     protected abstract void initialize();
 
     protected void show() {
+        logger.fine("Showing dialog window for detailed view of alias, tag colors or message.");
         isShowing = true;
         dialog.show();
     }
 
-    private void onClose(ActionEvent event) {
+    private void onClose() {
         isShowing = false;
         dialog.close();
     }
@@ -51,7 +60,8 @@ public abstract class UTDialog {
     @Subscribe
     private void handleKeyboardEscapeKeyPressedEvent(KeyboardEscapeKeyPressedEvent event) {
         if (isShowing) {
-            onClose(null);
+            logger.info(LogsCenter.getEventHandlingLogMessage(event));
+            onClose();
         }
     }
 }
