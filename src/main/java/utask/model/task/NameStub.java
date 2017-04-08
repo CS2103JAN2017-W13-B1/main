53b013fdbfbbec5470abde9083f66367ac0cce88
Denver Tan <revned.nat90@gmail.com>
2017-04-08 14:46:41 +0800

    Dependency Injection for Name

---
+++ src/main/java/utask/model/task/NameStub.java
@@ -0,0 +1,20 @@
+package utask.model.task;
+
+public class NameStub extends AbsName {
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+}
